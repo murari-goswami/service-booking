8cff68495123c376ed8f3d0462d2f039092b4886
Goswami, Murari <murari.goswami@adidas.com>
2020-01-05 14:27:41 +0100

    incremental

--- src/main/java/com/hairvision/booking/config/Swagger2Config.java
+++ src/main/java/com/hairvision/booking/config/Swagger2Config.java
@@ -2,37 +2,45 @@ package com.hairvision.booking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Hair Vision Saloon")
+                .description("Hair Vision Saloon User Interface")
+                .license("Shaina")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("", "", "shaina@gmail.com"))
+                .build();
+    }
+
     @Bean
     public Docket api() {
-        ParameterBuilder aparameterBuilder = new ParameterBuilder();
-        aparameterBuilder.name("Hair-Vision")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .defaultValue("Hair Vision")
-                .required(true)
-                .build();
-        java.util.List<Parameter> aParameters = new ArrayList<>();
-        aParameters.add(aparameterBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Hair-Vision")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .globalOperationParameters(aParameters);
+                .apiInfo(apiInfo());
     }
 }
 
