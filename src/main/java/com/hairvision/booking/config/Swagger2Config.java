b88950191fa003d2f43f8c5d255be16990ad1c6a
Goswami, Murari <murari.goswami@adidas.com>
2020-01-05 16:39:01 +0100

    incremental

--- src/main/java/com/hairvision/booking/config/Swagger2Config.java
+++ src/main/java/com/hairvision/booking/config/Swagger2Config.java
@@ -3,19 +3,13 @@ package com.hairvision.booking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -24,7 +18,7 @@ public class Swagger2Config {
     ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Hair Vision Saloon")
-                .description("Hair Vision Saloon User Interface")
+                .description("Hair Vision Saloon User Interface to track booking for customer")
                 .license("Shaina")
                 .termsOfServiceUrl("")
                 .version("1.0.0")
